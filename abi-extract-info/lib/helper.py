157bdcb945918b5d9262ce8168e715a8f055ca5d
Luis Silva <luiss@synopsys.com>
2024-07-23 09:12:12 -0700

    (snapshot) [lib/helper] Parsing and storing datatype test case details
    Handles parsing of standard output from the datatype
    signedness/size/align test case for use in other test cases.
    
    This information will be stored in `type_details` within
    the `TargetArch` class.
    
    Signed-off-by: Luis Silva <luiss@synopsys.com>

--- abi-extract-info/lib/helper.py
+++ abi-extract-info/lib/helper.py
@@ -17,3 +17,28 @@ def set_env(cc, sim):
     # Set environment variable
     os.environ["PATH"] = wrapper_path + f"/cc/{cc}:" + os.environ["PATH"]
     os.environ["PATH"] = wrapper_path + f"/sim/{sim}:" + os.environ["PATH"]
+
+import re
+# Responsible for parsing the standard output from the datatype
+#  signedness/size/align test case for use in other test cases.
+# This information will be stored in 'type_details' in the 'TargetArch' class.
+def parse_type_info(text):
+    lines = text.strip().split('\n')
+    type_details = dict()
+
+    for line in lines:
+        type_name = re.search(r'^([\w\s\*]+)\s*', line)
+        type_name = type_name.group(1) if type_name else None
+        type_name = type_name.strip()
+
+        signedness = re.search(r'signedness: (\d+)', line).group(1)
+        size = re.search(r'size: (\d+)', line).group(1)
+        align = re.search(r'align: (\d+)', line).group(1)
+
+        type_details[type_name] = {
+            "signedness": int(signedness),
+            "size": int(size),
+            "align": int(align)
+        }
+
+    return type_details
