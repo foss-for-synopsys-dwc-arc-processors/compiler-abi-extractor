e82be5272ac7046e90c7e83b9897b8d7de645f4a
Luis Silva <luiss@synopsys.com>
2024-08-05 02:26:58 -0700

    (snapshot) [lib/hexUtils] Fix __init__.
    Signed-off-by: Luis Silva <luiss@synopsys.com>

--- abi-extract-info/lib/hexUtils.py
+++ abi-extract-info/lib/hexUtils.py
@@ -13,6 +13,7 @@ functions for handling hexadecimal values.
 class HexUtils:
     def __init__(self, target, current_test):
         self.Target = target
+        self.current_test = current_test
 
     # Remove '0x' prefix.
     def _remove_identifier(self, value):
