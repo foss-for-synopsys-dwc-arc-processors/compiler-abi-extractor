6d88e7433760def0094d741366edf5869738a2bc
Luis Silva <luiss@synopsys.com>
2024-08-09 07:15:46 -0700

    (snapshot) [lib-hexUtils] Update sizeof() function as public.
    sizeof() function will be used outside their class, so
    removing the private symbol "_".
    
    Signed-off-by: Luis Silva <luiss@synopsys.com>

--- abi-extract-info/lib/hexUtils.py
+++ abi-extract-info/lib/hexUtils.py
@@ -34,7 +34,7 @@ class HexUtils:
         return difference
 
    # Calculate the number of bytes of `value`
-    def _sizeof(self, value):
+    def sizeof(self, value):
         hex_clean = value
         if "0x" in value:
             # Remove the '0x' prefix
@@ -108,7 +108,7 @@ class HexUtils:
         _value = _value * argc
 
         # Calculate the total number of bytes in the extended value.
-        total_byte_count = self._sizeof(_value)
+        total_byte_count = self.sizeof(_value)
         remains = []
 
         # Split the extended value into chunks of `int_byte_count` bytes.
@@ -209,7 +209,7 @@ class HexUtils:
         # Process `argv` to find matching register values.
         while argv:
             value = argv.pop(0)
-            sizeof_value = self._sizeof(value)
+            sizeof_value = self.sizeof(value)
 
             # Split the value if its size exceeds `sizeof_int`
             if sizeof_value > sizeof_int:
@@ -241,18 +241,18 @@ class HexUtils:
             res = []
 
             # Check if the current value is already of the expected size.
-            if self._sizeof(value) == sizeof_int:
+            if self.sizeof(value) == sizeof_int:
                 argv.pop(0)
                 continue
 
             # Aggregate values until the combined size reaches or exceeds sizeof_int
-            while argv and self._sizeof(self._combine_hex_values(res)) < sizeof_int:
+            while argv and self.sizeof(self._combine_hex_values(res)) < sizeof_int:
                 res.append(argv.pop(0))
 
                 if argv:
                     next_value = argv[0]
                     # Check if combining with the next value fits within `sizeof_int`
-                    if self._sizeof(self._combine_hex_values(res)) + self._sizeof(next_value) <= sizeof_int:
+                    if self.sizeof(self._combine_hex_values(res)) + self.sizeof(next_value) <= sizeof_int:
                         res.append(argv.pop(0))
                     else:
                         break
@@ -260,7 +260,7 @@ class HexUtils:
             # Special case for combining values - `char`, `short`
             # When we are passing a `struct {char, short}`, the char value will be extended
             # to align with short.
-            if len(res) == 2 and self._sizeof(res[0]) == 1 and self._sizeof(res[1]) == 2:
+            if len(res) == 2 and self.sizeof(res[0]) == 1 and self.sizeof(res[1]) == 2:
                 res[0] = f"0x00{res[0][2:]}"  # Adjust representation for short exception.
 
             # Combine the aggregated values into a single hexadecimal value.
@@ -296,11 +296,11 @@ class HexUtils:
             # compiler might use another of the argument registers to store a stack address to load
             # the values into the first argument register before the actual function call.
             # i.e it first stores the values into the stack, then loads it to register.
-            if register_values_dict["x10"] == stack_address:
+            if register_values_dict["a0"] == stack_address:
                 # FIXME Here it would make sense to validate the next stack value,
                 # but has been observe that the next value is not always in the next stack address.
                 if stack_value == argv[0]:
-                    citeration["passed_by_ref_register"] = ["x10"] # FIXME hardcoded..
+                    citeration["passed_by_ref_register"] = ["a0"] # FIXME hardcoded..
                     diff = self._hex_difference(stack[0][0], stack[index][0])
                     citeration["passed_by_ref"] = f"sp + {diff}"
 
@@ -324,7 +324,7 @@ class HexUtils:
             value = argv.pop(0)
 
             # Get the size of the current value.
-            sizeof_value = self._sizeof(value)
+            sizeof_value = self.sizeof(value)
 
             # Check if makes sense to split the value.
             if sizeof_value <= sizeof_int:
@@ -342,11 +342,11 @@ class HexUtils:
                 # compiler might use another of the argument registers to store a stack address to load
                 # the values into the first argument register before the actual function call.
                 # i.e it first stores the values into the stack, then loads it to register.
-                if register_values_dict["x10"] == stack_address:
+                if register_values_dict["a0"] == stack_address:
                     # FIXME Here it would make sense to validate the next stack value,
                     # but has been observe that the next value is not always in the next stack address.
                     if stack_value == first_half or stack_value == second_half:
-                        citeration["passed_by_ref_register"] = ["x10"] # FIXME harcoded..
+                        citeration["passed_by_ref_register"] = ["a0"] # FIXME harcoded..
                         diff = self._hex_difference(stack[0][0], stack[index][0])
                         citeration["passed_by_ref"] = f"sp + {diff}"
 
@@ -372,13 +372,13 @@ class HexUtils:
             value = argv[0]
             res = []
             # Aggregate values until the combined size reaches or exceeds sizeof_int.
-            while argv and self._sizeof(self._combine_hex_values(res)) < sizeof_int:
+            while argv and self.sizeof(self._combine_hex_values(res)) < sizeof_int:
                 res.append(argv.pop(0))
 
                 # Check if adding the next would still fit within `sizeof_int`.
                 if argv:
                     next_value = argv[0]
-                    if self._sizeof(self._combine_hex_values(res)) + self._sizeof(next_value) <= sizeof_int:
+                    if self.sizeof(self._combine_hex_values(res)) + self.sizeof(next_value) <= sizeof_int:
                         res.append(argv.pop(0))
                     else:
                         break
@@ -386,7 +386,7 @@ class HexUtils:
             # Special case for combining values - `char`, `short`
             # When we are passing a `struct {char, short}`, the char value will be extended
             # to align with short.
-            if len(res) == 2 and self._sizeof(res[0]) == 1 and self._sizeof(res[1]) == 2:
+            if len(res) == 2 and self.sizeof(res[0]) == 1 and self.sizeof(res[1]) == 2:
                 res[0] = f"0x00{res[0][2:]}"  # Adjust representation for short exception.
 
             # Combine the aggregated values into a single hexadecimal value.
@@ -400,9 +400,9 @@ class HexUtils:
                 # Here instead of checking all positions in the stack,
                 # we only care about the first one.
                 stack_address, stack_value = stack[index]
-                if register_values_dict["x10"] == stack_address:
+                if register_values_dict["a0"] == stack_address:
                     if stack_value == combined_hex:
-                        citeration["passed_by_ref_register"] = ["x10"]
+                        citeration["passed_by_ref_register"] = ["a0"]
                         diff = self._hex_difference(stack[0][0], stack[index][0])
                         citeration["passed_by_ref"] = f"sp + {diff}"
 
