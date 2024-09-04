e3c42f3e1aba91815ec036f1f58d453b2afd7df8
Luis Silva <luiss@synopsys.com>
2024-09-04 14:39:39 -0700

    (snapshot) [lib/targetArch] Add RISCV floating point registers.
    Add regs_bank1 as floating point registers support for those
    architectures that support it.
    
    Signed-off-by: Luis Silva <luiss@synopsys.com>

--- abi-extract-info/lib/targetArch.py
+++ abi-extract-info/lib/targetArch.py
@@ -8,12 +8,19 @@
 class TargetArch:
     def __init__(self):
         self.type_details = dict()
+        self.argument_registers_2 = dict()
 
     def set_type_details(self, type_details):
         self.type_details = type_details
 
     def get_type_details(self, datatype):
         return self.type_details[datatype]
+
+    def set_argument_registers_2(self, dtype, registers):
+        self.argument_registers_2[dtype] = registers
+
+    def get_argument_registers_2(self, dtype):
+        return self.argument_registers_2[dtype]
 
     def get_registers(self):
         raise NotImplementedError("Subclasses should implement this!")
@@ -27,20 +34,36 @@ class TargetArch:
 # RISCV target specific
 class RISCV(TargetArch):
     def __init__(self):
-        self.Registers = [
-            "zero", "ra", "sp", "gp", "tp",
-            "t0", "t1", "t2", "s0", "s1",
-            "a0", "a1", "a2", "a3", "a4",
-            "a5", "a6", "a7", "s2", "s3",
-            "s4", "s5", "s6", "s7", "s8",
-            "s9", "s10", "s11", "t3", "t4",
-            "t5", "t6"
-        ]
+        super().__init__()
+        self.Registers = {
+            "regs_bank0": [
+                "zero", "ra", "sp", "gp", "tp",
+                "t0", "t1", "t2", "s0", "s1",
+                "a0", "a1", "a2", "a3", "a4",
+                "a5", "a6", "a7", "s2", "s3",
+                "s4", "s5", "s6", "s7", "s8",
+                "s9", "s10", "s11", "t3", "t4",
+                "t5", "t6"
+            ],
+            "regs_bank1": [
+                "ft0", "ft1", "ft2", "ft3", "ft4",
+                "ft5", "ft6", "ft7", "fs0", "fs1",
+                "fa0", "fa1", "fa2", "fa3", "fa4",
+                "fa5", "fa6", "fa7", "fs2", "fs3",
+                "fs4", "fs5", "fs6", "fs7", "fs8",
+                "fs9", "fs10", "fs11", "ft8", "ft9",
+                "ft10", "ft11"
+            ]
+        }
+
         # According to RISCV ABI.
         self.ArgumentRegisters = ["a0", "a1", "a2", "a3",
                                   "a4", "a5", "a6", "a7"]
 
-    def get_registers(self):
+    def get_registers(self, key = None):
+        if key and key in self.Registers:
+            return self.Registers[key]
+
         return self.Registers
 
     def get_argument_registers(self):
