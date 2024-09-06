2c1895e337bff49f022bd5d97ecb8046095b6308
Luis Silva <luiss@synopsys.com>
2024-09-06 13:05:54 -0700

    (snapshot) [lib/structTests] Refactor summary report generator.
    The updated report now includes more detailed information
    about which registers are used for different data types.
    
    Previous implementation:
    
        Struct passing test:
        - sizeof(S) <= 8 : passed in registers: a0, a1, fa0, fa1
        - sizeof(S) >  8 : passed by ref: [stack]
          - float : int : long - as fill
          - char : short - as combined
          - double : long long - as pairs
        - empty struct is ignored by C compiler.
    
    New implementation:
    
        Struct argument passing test:
        - sizeof(S) <= 8 : passed in registers
        - sizeof(S) >  8 : passed by ref: [stack]
          - char : short : int : long : a0, a1
          - long long [low], [high]: a0, a1
          - float : fa0, fa1
          - double : fa0
        - empty struct is ignored by C compiler.
    
    Signed-off-by: Luis Silva <luiss@synopsys.com>

--- abi-extract-info/lib/structTests.py
+++ abi-extract-info/lib/structTests.py
@@ -17,94 +17,87 @@ class StructTests:
         self.Target = Target
         self.results = []
 
-    # Populate the "fill" type register entries for a given data type.
-    def handle_registers_fill(self, iteration, dtype, types, passed_by_ref):
-        for reg in iteration.get("registers_fill", []):
-            types["fill"].setdefault(reg, []).append(dtype)
-        # If the iteration is marked as passed by reference, store it.
-        if iteration.get("passed_by_ref"):
-            passed_by_ref[iteration["passed_by_ref"]] = ""
-
-    # Polupate the "combined" type register entries for a given data type.
-    def handle_registers_combined(self, iteration, dtype, types):
-        for reg in iteration.get("registers_combined", []):
-            types["combined"].setdefault(reg, []).append(dtype)
-
-    # Polupate the "pairs" type register entries for a given data type.
-    def handle_registers_pairs(self, iteration, dtype, types):
-        for reg in iteration.get("registers_pairs", []):
-            types["pairs"].setdefault(reg, []).append(dtype)
-
-    # Process the results to categorize data types based on how
-    # they are passed (in registers or by reference), and determines boundary values.
+    # Process the results to categorize them according to their registers
+    # and if values were splitted in pairs.
     def process_results(self, results):
-        types = { "fill": {}, "combined": {}, "pairs": {}}
+        types = []
         passed_by_ref = {}
         boundary = {}
 
-        # Determine the register size.
+        # Get the size of a registers.
         register_size = self.Target.get_type_details("int")["size"]
         for dtype, iterations in results.items():
-            for iteration in iterations:
-                sizeof_s = iteration["sizeof(S)"]
+            if len(iterations) > 1:
+                iteration = iterations[-2]
 
-                # Get the size of the current data type.
+                # Get the size of the current datatype
                 sizeof_dtype = self.Target.get_type_details(dtype)["size"]
 
-                # Categorize based on the size comparison with register sizeof.
-                if sizeof_dtype == register_size:
-                    self.handle_registers_fill(iteration, dtype, types, passed_by_ref)
-                elif sizeof_dtype < register_size:
-                    self.handle_registers_combined(iteration, dtype, types)
-                elif sizeof_dtype > register_size:
-                    self.handle_registers_pairs(iteration, dtype, types)
-                    if "pairs_order" in iteration:
-                        pairs_order = iteration["pairs_order"]
+                if not types:
+                    if sizeof_dtype == register_size:
+                        types.append({ "dtypes": [dtype], "regs": list(iteration["registers_fill"].keys()), "pairs": ""})
+                    elif sizeof_dtype < register_size:
+                        types.append({ "dtypes": [dtype], "regs": list(iteration["registers_combined"].keys()), "pairs": ""})
+                    elif sizeof_dtype > register_size:
+                        types.append({ "dtypes": [dtype], "regs": list(iteration["registers_pairs"].keys()), "pairs": iteration["pairs_order"]})
+                    continue
 
-            # Track the boundary vakyes based on the sizeof(S) for the second last iteration.
+                # Get the registers according to the size type.
+                if sizeof_dtype == register_size:
+                    regs = list(iteration["registers_fill"].keys())
+                elif sizeof_dtype < register_size:
+                    regs = list(iteration["registers_combined"].keys())
+                elif sizeof_dtype > register_size:
+                    regs = list(iteration["registers_pairs"].keys())
+                pairs = iteration["pairs_order"]
+
+                found = False
+                for x in types.copy():
+                    # Aggregate the dtype according to their registers
+                    # used and if they are in pairs.
+                    if regs == x["regs"] and pairs == x["pairs"]:
+                        x["dtypes"].append(dtype)
+                        found = True
+                        break
+
+                if not found:
+                    types.append({ "dtypes": [dtype], "regs": regs, "pairs": pairs })
+
+            # Track the boundary values based on the sizeof(S) for the second last iteration.
             if len(iterations) >= 2:
                 boundary[iterations[-2]["sizeof(S)"]] = ""
+                passed_by_ref[iterations[-1]["passed_by_ref"]] = ""
 
         # Get the voundary value and the pass-by-ref value.
         boundary_value = next(iter(boundary.keys()), None)
         passed_by_ref_value = next(iter(passed_by_ref.keys()), None)
 
-        return types, boundary_value, passed_by_ref_value, pairs_order
 
-    # Create a summary of how different data types structs are passed,
-    # categorizing them as "fill", "combined", "pairs".
-    def summary_results(self, types, boundary_value, passed_by_ref_value, pairs_order):
-        # Collect all unique registers.
-        registers = list(set(types["fill"]) | set(types["combined"]) | set(types["pairs"]))
+        return types, boundary_value, passed_by_ref_value
+
+    # Create a summary
+    def summary_results(self, types, boundary_value, passed_by_ref_value):
         summary = [
-            "Struct passing test:",
-            f"- sizeof(S) <= {boundary_value} : passed in registers: {', '.join(map(str, registers))}",
+            "Struct argument passing test:",
+            f"- sizeof(S) <= {boundary_value} : passed in registers",
             f"- sizeof(S) >  {boundary_value} : passed by ref: {passed_by_ref_value}"
         ]
+        for i in types:
+            dtypes_str = ' : '.join(i["dtypes"])
+            regs_str   = ', '.join(i["regs"])
+            pairs_str  = i["pairs"]
+            summary.append(f"  - {dtypes_str} {pairs_str}: {regs_str}")
 
-        # Organize the data types under each category.
-        dtypes = {
-            key: list({dtype for values in dtype_list.values() for dtype in values})
-            for key, dtype_list in types.items()
-        }
-
-        # Append the categorized data types to the summary.
-        summary.extend([
-            f"  - {' : '.join(sorted(dtypes['fill']))} - as fill",
-            f"  - {' : '.join(sorted(dtypes['combined']))} - as combined",
-            f"  - {' : '.join(sorted(dtypes['pairs']))} - as pairs {pairs_order}",
-            f""
-        ])
-
+        summary.append("")
         return summary
 
     # Prepare the summary based on the test case results.
     def prepare_summary(self, results):
         # process the results
-        types, boundary_value, passed_by_ref_value, pairs_order = self.process_results(results)
+        types, boundary_value, passed_by_ref_value = self.process_results(results)
 
         # Summarize and output the results
-        summary = self.summary_results(types, boundary_value, passed_by_ref_value, pairs_order)
+        summary = self.summary_results(types, boundary_value, passed_by_ref_value)
         return "\n".join(summary)
 
     # Run the test to check if the value is in registers or the stack.
