f646239c43f58a085ea6e050fc86392bef408a87
Luis Silva <luiss@synopsys.com>
2025-01-15 06:41:48 -0800

    (snapshot) [src/helper] Add conditional compilation for FPU information.
    Only dump information about the second register
    bank if FPU is enabled by the `CONFIG_FPU` macro.
    
    Signed-off-by: Luis Silva <luiss@synopsys.com>

--- abi-extract-info/src/helper.c
+++ abi-extract-info/src/helper.c
@@ -11,7 +11,9 @@
 #include <stdio.h>
 
 extern unsigned regs_bank0[32];
+#if CONFIG_FPU
 extern unsigned long long regs_bank1[32];
+#endif
 
 #define ARRAY_LENGTH(x) sizeof(x)/sizeof(x[0])
 
@@ -21,19 +23,29 @@ void dump_information(unsigned* Stack) {
     printf("// Header info\n");
     printf("%p\n0x%08x\n", Stack, sizeof(Stack));
     // Number of register bancks
+#if CONFIG_FPU
+    printf("0x2\n");
+#else
     printf("0x1\n");
+#endif
     // bank0:  ID, size, number of regs
     printf("0x%x\n0x%x\n0x%x\n", /*ID*/ 0, sizeof(regs_bank0[0]), /*number of regs*/ ARRAY_LENGTH(regs_bank0));
+#if CONFIG_FPU
+    // bank1:  ID, size, number of regs
+    printf("0x%x\n0x%x\n0x%x\n", /*ID*/ 1, sizeof(regs_bank0[1]), /*number of regs*/ ARRAY_LENGTH(regs_bank1));
+#endif
 
     // Dump register bank0: regs_bank0
     printf("// regs_bank0\n");
     for(unsigned i=0; i<ARRAY_LENGTH(regs_bank0); ++i)
        printf("0x%x\n", regs_bank0[i]);
 
+#if CONFIG_FPU
     // Dump register bank1: regs_bank1
     printf("// regs_bank1\n");
     for(unsigned i=0; i<ARRAY_LENGTH(regs_bank1); ++i)
        printf("0x%llx\n", regs_bank1[i]);
+#endif
 
     // stack: 32 entries (128 bytes on 32bit system)
     printf("// Start of stack dump: %p\n", Stack);
