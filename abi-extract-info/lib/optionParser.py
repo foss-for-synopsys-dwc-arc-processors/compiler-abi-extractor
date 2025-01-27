890d0b4952d065f5cf3bbf783b108b6faeff492e
Luis Silva <luiss@synopsys.com>
2025-01-27 06:26:34 -0800

    [lib/optionParser] Validate selected compiler and simulator.
    Ensure that the chosen compiler (-cc) and simulator (-sim) options
    correspond to available wrappers in "./scripts/wrappers/{cc,sim}" during
    command-line execution.
    
    Signed-off-by: Luis Silva <luiss@synopsys.com>

--- abi-extract-info/lib/optionParser.py
+++ abi-extract-info/lib/optionParser.py
@@ -22,31 +22,81 @@ class OptionParser:
     def set(self, name, default_value=True):
         self.flags[name] = default_value
 
+    def set_configuration(self, tool, value):
+        if not value:
+            print(f"fatal: {tool.upper()} configuration not provided.")
+            exit(1)
+        configurations = self.get_available_configuration(tool)
+        if value not in configurations:
+            print(f"fatal: {tool.upper()} configuration {value} not found.")
+            exit(1)
+        self.flags[tool] = value
+
     def get(self, name):
         return self.flags.get(name, False)
 
+    def get_available_configuration(self, tool):
+        tool_path = os.path.join("scripts/wrapper", tool)
+        contents = os.listdir(tool_path)
+        return contents
+
+    def display_available_configuration(self, tool):
+        configurations = self.get_available_configuration(tool)
+        print(f"Available configurations for {tool.upper()}:")
+        for item in configurations:
+            print(f"- {item}")
+
     def helper(self):
-        return
+        helper_message = \
+"""Usage python3 abi-extract-info.py [options]
+Options:
+  -cc <compiler wrapper>        Select the compiler.
+  -sim <simulator wrapper>      Select the simulator.
+  -v | --verbose                Print execution commands.
+  --print-report                Print summary report upon conclusion.
+  --save-temps                  Do not delete the temporary files from "tmp/" directory.
+  --help                        Display this information.
+  --help=cc                     Display available compiler options.
+  --help=sim                    Display available simulator options.
+"""
+        print(helper_message)
+        exit(0)
 
     def set_default(self):
         self.set("verbose", False)
 
-    def option_parser(self, args = sys.argv):
-        self.set_default()
-        args.pop(0)
-        while args:
-            arg = args.pop(0)
-            if arg == "-h" or arg == "--help":
-                self.help()
-            elif arg == "-cc" or arg == "--compiler":
-                compiler = args.pop(0)
-                self.set("cc", compiler)
-            elif arg == "-sim" or arg == "--simulator":
-                simulator = args.pop(0)
-                self.set("sim", simulator)
-            elif arg == "-v" or arg == "--verbose":
-                self.set("verbose")
-            elif arg == "--print-report":
-                self.set("print-report")
-            elif arg == "--save-temps":
-                self.set("save_temps")
+    def option_parser(self, args = sys.argv[1:]):
+            self.set_default()
+
+            actions = {
+                "-cc":              lambda: self.set_configuration("cc",  next(arg_iter, None)),
+                "--compiler":       lambda: self.set_configuration("cc",  next(arg_iter, None)),
+                "-sim":             lambda: self.set_configuration("sim", next(arg_iter, None)),
+                "--simulator":      lambda: self.set_configuration("sim", next(arg_iter, None)),
+                "-v":               lambda: self.set("verbose"),
+                "--verbose":        lambda: self.set("verbose"),
+                "--print-report":   lambda: self.set("print-report"),
+                "--save-temps":     lambda: self.set("save_temps"),
+            }
+
+            help_options = {
+                "-h":           self.helper,
+                "--help":       self.helper,
+                "--help=cc":    lambda: self.display_available_configuration("cc"),
+                "--help=sim":   lambda: self.display_available_configuration("sim"),
+            }
+
+            to_exit = False
+            arg_iter = iter(args)
+            for arg in arg_iter:
+                if arg in help_options:
+                    help_options[arg]()
+                    to_exit = True
+                elif arg in actions:
+                    actions[arg]()
+                else:
+                    print(f"fatal: Unknown argument: {arg}")
+                    exit(1)
+
+            if to_exit:
+                exit(0)
